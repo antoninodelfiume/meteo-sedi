08a6496f60f982149a872a0d27c0175a6212d328
Antonino Del Fiume <antonino.delfiume@bsi-software.com>
2026-07-23 11:13:28 +0200

    TODO 03 - bis

--- src/weather/state/WeatherWorkspaceContext.tsx
+++ src/weather/state/WeatherWorkspaceContext.tsx
@@ -42,8 +42,8 @@ export function WeatherWorkspaceProvider({ children }: PropsWithChildren) {
         selectedSiteId,
         selectedSite,
         temperatureUnit,
-        selectSite: () => undefined,
-        setTemperatureUnit: () => undefined,
+        selectSite,
+        setTemperatureUnit,
       }}
     >
       {children}
